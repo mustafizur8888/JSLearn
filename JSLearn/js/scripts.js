dee1e731ec50ae573a375fd160278863a6335f3a
Mustafizur Rahman <Mustafizur Rahman@MUSTAFIZUR>
2016-12-31 20:04:49 +0600

    Add project files.

---
+++ JSLearn/js/scripts.js
@@ -0,0 +1,24 @@
+﻿var ourHeadline = document.querySelector("#our-headline");
+var ourButton = document.querySelector("#our-button");
+var itemAdd = 0;
+
+var listItems = document.querySelector("#ourList");
+listItems.addEventListener("click", listItemClick);
+ourButton.addEventListener("click", createNewItem);
+
+
+function createNewItem() {
+    listItems.innerHTML += "<li class= " + " list-group-item  > SomeThing New " + ++itemAdd + "</li>";
+}
+
+function listItemClick(e) {
+
+    if (e.target.nodeName === "LI") {
+
+        for (var i = 0; i < e.target.parentNode.children.length; i++) {
+            e.target.parentNode.children[i].classList.remove("active");
+        }
+        ourHeadline.innerHTML = e.target.innerHTML;
+        e.target.classList.add("active");
+    }
+}
